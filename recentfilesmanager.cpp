ccbc115b67bff7b7beb69c4de93b0dbbc139cce1
羊小艾41 <102524131+18768794292@users.noreply.github.com>
2024-01-11 23:22:19 +0800

    最近历史修改

--- recentfilesmanager.cpp
+++ recentfilesmanager.cpp
@@ -1,18 +1,18 @@
-// recentfilesmanager.cpp
+
 #include "recentfilesmanager.h"
 #include <QSettings>
 
 RecentFilesManager::RecentFilesManager(QObject *parent)
     : QObject(parent)
 {
-    // 从设置中读取最近文件列表
+
     QSettings settings("MyCompany", "MyApp");
     recentFilesList = settings.value("recentFiles").toStringList();
 }
 void RecentFilesManager::addRecentFile(const QString &filePath)
 {
-    // 添加最近文件
-    recentFilesList.removeAll(filePath); // 避免重复
+
+    recentFilesList.removeAll(filePath);
     recentFilesList.prepend(filePath);
     while (recentFilesList.size() > maxRecentFiles)
     {
